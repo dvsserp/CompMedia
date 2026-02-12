6f4087407aa8e0e3f0ad96c4485f35cf269def5e
Alvin Hu <alvinhu8@gmail.com>
2026-02-11 21:14:38 -0500

    fixed paint palette

--- paint.py
+++ paint.py
@@ -1,4 +1,5 @@
 color1 = {}
+c = None
 def setup():
     size(900,800)
     global color1
@@ -12,34 +13,19 @@ def setup():
         "purple" : 270,
         "pink" : 300
         }
-    
 def draw():
+    global c
     color_mode(HSB, 360, 100, 100)
     n = 50
     for i in color1:
+        stroke(0,0,0)
         fill(color1[i], 100, 100)
         square(n, 700, 50)
         n += 100
-    c = 0
-    if collidePointRect(mouse_x, mouse_y, 50, 700, 50,50) and is_mouse_pressed:
-        c = color(color1["red"], 100, 100)
-    elif collidePointRect(mouse_x, mouse_y, 150, 700, 50,50) and is_mouse_pressed:
-        c = color(color1["orange"], 100, 100)
-    elif collidePointRect(mouse_x, mouse_y, 250, 700, 50,50) and is_mouse_pressed:
-        c = color(color1["yellow"], 100, 100)
-    elif collidePointRect(mouse_x, mouse_y, 350, 700, 50,50) and is_mouse_pressed:
-        c = color(color1["green"], 100, 100)
-    elif collidePointRect(mouse_x, mouse_y, 450, 700, 50,50) and is_mouse_pressed:
-        c = color(color1["cyan"], 100, 100)
-    elif collidePointRect(mouse_x, mouse_y, 550, 700, 50,50) and is_mouse_pressed:
-        c = color(color1["blue"], 100, 100)
-    elif collidePointRect(mouse_x, mouse_y, 650, 700, 50,50) and is_mouse_pressed:
-        c = color(color1["purple"], 100, 100)
-    elif collidePointRect(mouse_x, mouse_y, 750, 700, 50,50) and is_mouse_pressed:
-        c = color(color1["pink"], 100, 100)
-    if is_mouse_pressed:
-        fill(c)
-        line(mouse_x, mouse_y, pmouse_x, pmouse_y)
+    
+    if is_mouse_pressed and mouse_y < 700:
+        stroke(c)
+        line(mouse_x,mouse_y,pmouse_x,pmouse_y)
             
 def collidePointRect(pX, pY, rX, rY, rW, rH):
   """Input x,y coordinates of point and x, y, width, and height of rectangle.
@@ -49,3 +35,11 @@ def collidePointRect(pX, pY, rX, rY, rW, rH):
     return True
   else:
     return False
+
+def mouse_pressed():
+    global c
+    x = 50
+    for name in color1:
+        if(collidePointRect(mouse_x, mouse_y, x, 700,50,50)):
+           c = color(color1[name],100,100)
+        x += 100
