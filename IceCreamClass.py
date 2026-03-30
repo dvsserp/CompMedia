3b9aa86c9cd5e46838fa4a8a6ae468f2a2bf066c
Alvin <alvinhu8@gmail.com>
2026-03-30 10:15:25 -0400

    update

--- IceCreamClass.py
+++ IceCreamClass.py
@@ -1,27 +1,21 @@
 # PY5 IMPORTED MODE CODE
 class IceCream:
-    def __init__(self, color, cone, scoops):
+    def __init__(self, color, cone, scoops,topping):
         self.x = 0
         self.y = 0
         self.color = color
         self.cone = cone
         self.scoops = scoops
+        self.toppings = topping
         
     def drawIceCream(self):
         color_mode(HSB,360,100,100)
         fill(self.color,100,100)
         no_stroke()
-        for i in range(1, self.scoops*30, 30):
-            circle(self.x, self.y-i, 40)
+        for i in range(1, self.scoops*50, 50):
+            circle(self.x, self.y-i, 80)
         fill(28,72,41)
         if(self.cone):
-            triangle(self.x-20,self.y+10,self.x+20,self.y+10, self.x, self.y+60)
+            triangle(self.x-40,self.y+20,self.x+40,self.y+20, self.x, self.y+120)
         else:
-            rect(self.x-20,self.y+10,40,30)
-        
-    def animateText(self, i):
-        no_stroke()
-        #h
-        if(i < 50):
-            circle(self.x - (width/4), self.y + i - (height/2 - 50), 10)
-            i += 5
+            rect(self.x-40,self.y+20,80,60)
--- holiday.py
+++ holiday.py
@@ -10,17 +10,18 @@ colors = {
     "blue":210,
     "purple":270,
     "pink":300}
-
+toppings = ["None","Sprinkles", "Gummies", "Cherry", "Chocolate"] 
 def setup():
     size(1000,1000)
     color_mode(HSB,360,100,100)
 def draw():
-    global colors
+    global colors,toppings
     translate(width/2,height/2)
-    ice1 = IceCream(colors["red"], True, 2)
+    ice1 = IceCream(colors["red"], False, 2, toppings[0])
     ice1.drawIceCream()
-    drawHAPPY()
     drawDAY()
+    drawHAPPY()
+    Palette()
     
 def drawHAPPY():
     global movex, movey
@@ -50,22 +51,45 @@ def drawHAPPY():
         circle(leftSideHappy + 280 + movex/2, happyHeight + movey/2, 10)
         circle(leftSideHappy + 330 - movex/2, happyHeight + movey/2, 10)
         circle(leftSideHappy + 305, happyHeight + 30 + movey/2,10)
+        
+        
     movex += 1
     movey += 1
 
 def drawDAY():
     global movex,movey
-    leftSideDay = width/2
-    dayHeight = height - height/6
+    leftSideDay = -width / 5
+    dayHeight = height/8
     if(movey < 50):
+        #D
+        arc(leftSideDay + 150, dayHeight+25, 80,60,radians(270),radians(270+180),CHORD)
+        
         #A
-        circle(leftSideDay+ movex/2 + 100, dayHeight - movey + 50,10)
-        circle(leftSideDay + movex/2 + 125, dayHeight + movey,10)
-        circle(leftSideDay + movex/2 + 110, dayHeight + 25, 10)
+        circle(leftSideDay+ movex/2 + 200, dayHeight - movey + 50,10)
+        circle(leftSideDay + movex/2 + 225, dayHeight + movey,10)
+        circle(leftSideDay + movex/2 + 210, dayHeight + 25, 10)
     
         #Y
         circle(leftSideDay + 280 + movex/2, dayHeight + movey/2, 10)
         circle(leftSideDay + 330 - movex/2, dayHeight + movey/2, 10)
         circle(leftSideDay + 305, dayHeight + 30 + movey/2,10)
-    
+        
+        
+def Palette():
+    global colors, toppings
+    fill(255)
+    text("Topping", 0, height/2 - 230)
+    for i in range(0,len(toppings)*200,200):
+        rect(-width/2 + i + 60, height/2 - 200, 70,50)
+    for i in range(0, len(colors)*120,120):
+        rect(-width/2 + i + 30, height/2 - 120, 70, 50)
+        
+def collidePointRect(pX, pY, rX, rY, rW, rH):
+  """Input x,y coordinates of point and x, y, width, and height of rectangle.
+  Returns true if the point and rectangle are touching."""
+
+  if pX >= rX and pX <= rX + rW and pY >= rY and pY <= rY + rH:
+    return True
+  else:
+    return False
     
