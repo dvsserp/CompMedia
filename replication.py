8efc377c0cb5dfd783d867d748ade9f01433d8c5
Alvin Hu <alvinhu8@gmail.com>
2026-05-13 23:16:50 -0400

    tried

--- replication.py
+++ replication.py
@@ -2,11 +2,12 @@ import numpy as np
 def setup():
     size(200,200)
     background(0)
+    initState()
 def changeState(x,y):
     if(np_pixels[y,x,1] == 255):
-        np_pixels[y,x,:] == [100,0,0,0]
+        np_pixels[y,x,:] = [100,0,0,0]
     else:
-        np_pixels[y,x,1] = [100,255,0,0]
+        np_pixels[y,x,:] = [100,255,0,0]
     update_np_pixels()
     
 def initState():
@@ -17,12 +18,34 @@ def initState():
     update_np_pixels()
     
 def checkNeighbor():
+    load_np_pixels()
     BLACK = [100,0,0,0]
     RED = [100,255,0,0]
-    count = 0
-    for x in range(width):
-        for y in range(height):
-            if(np_pixels[y,x,:] == RED):
-                count += 1
+    for x in range(width-1):
+        for y in range(height-1):
+            count = 0
+            if np.array_equal(np_pixels[y,x,:], BLACK):
+                if np.array_equal(np_pixels[y,x-1,:], RED):
+                    count += 1
+                if np.array_equal(np_pixels[y,x+1,:],RED):
+                    count += 1
+                if np.array_equal(np_pixels[y-1,x,:], RED):
+                    count += 1
+                if np.array_equal(np_pixels[y+1,x,:], RED):
+                    count += 1
+                if (count%2 == 1):
+                    changeState(x,y)
+            else:
+                if np.array_equal(np_pixels[y,x-1,:], RED):
+                    count += 1
+                if np.array_equal(np_pixels[y,x+1,:], RED):
+                    count += 1
+                if np.array_equal(np_pixels[y-1,x,:], RED):
+                    count += 1
+                if np.array_equal(np_pixels[y+1,x,:], RED):
+                    count += 1
+                if(count%2 == 0):
+                    changeState(x,y)
+    update_np_pixels()
 def draw():
-    initState()
+    checkNeighbor()
